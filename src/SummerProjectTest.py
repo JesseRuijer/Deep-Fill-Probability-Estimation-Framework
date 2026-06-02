3e97f550dfa81fa46e01765cb6fe18f702407c78
GOAT <Jesse.ruijer@gmail.com>
2026-06-02 16:52:20 +0200

    02/06

--- src/SummerProjectTest.py
+++ src/SummerProjectTest.py
@@ -11,7 +11,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
-from sklearn.metrics import confusion_matrix, classification_report, precision_recall_curve, auc
+from sklearn.metrics import confusion_matrix, classification_report, precision_recall_curve, roc_auc_score, brier_score_loss, log_loss
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+
+from Functions import time_in_hours, plots, plot_feature, plot_corr_map
+
 
 pd.set_option('display.max_columns', None)
 
@@ -127,7 +133,7 @@ def data_regressors(rawdata, cleandata):
     #below fill.na(0) means it will fill imbalcne with 0 if theres a NaN situation
     #Q imbalance is just using the best bid and ask volumes
     Regressors_df["QImbalance"] = ((df_BV[0]-df_SV[0])/(df_BV[0]+ df_SV[0])).fillna(0)
-
+    Regressors_df["AbsQImbalance"] = Regressors_df["QImbalance"].abs()
     #Total Vol imbalance uses sum of the 20 cols provided in the data
     #axis=1 does across cols, axis=0 does across rows
     Regressors_df["TotalVolImbalance"] = ((df_BV.sum(axis=1)-df_SV.sum(axis=1))/(df_BV.sum(axis=1)+ df_SV.sum(axis=1))).fillna(0)
@@ -138,6 +144,10 @@ def data_regressors(rawdata, cleandata):
     Regressors_df["Weighted Vol Imbalance"] = (((weights*df_BV).sum(axis=1)-(weights*df_SV).sum(axis=1))/((weights*df_BV).sum(axis=1)+ (weights*df_SV).sum(axis=1))).fillna(0)
     Regressors_df["Midprice"] = (df_BP[0]+df_SP[0])/2
     Regressors_df["Microprice"] = ((df_BV[0]*df_SP[0])+(df_SV[0]*df_BP[0]))/(df_BV[0]+df_SV[0])
+    
+    
+    #Calculates how far order is awaay from best bid or best ask
+    Regressors_df['distance_to_best_price_ask'] = cleandata['SellPrice'][0] - cleandata[]
     
     train_mask = (
             (df_E2["Type"] != 88) &
@@ -155,9 +165,6 @@ def data_regressors(rawdata, cleandata):
 
     #Converts it to binary 1 for fill to use in logistic regression
     fill_map = fill_map.astype(int)
-
-    #Below may or may not be necessary since this is matlab data to force the indices to be integers
-    #fill_map.index = fill_map.index.astype(int)
 
     #Now map it to my Regressors df feature matrix i made above
 
@@ -177,15 +184,46 @@ print(f" Total number of events on 1 April 2014 of INTC is {len(cleandata['Event
 print(f" Amount of Market Orders on 1 April 2014 of INTC is {len(cleandata['MO'])}")
 print(f' Percentage of MO per total number of events on 1 April 2014 INTC is {(len(cleandata["MO"])/len(cleandata["Event"])*100):.04f}%')
 
-##order that dont walk the book are just orders that have same avg price as best price
+buy_no_walk = (cleandata['MO']['APPS'] == cleandata['MO']['BAP'] ) & (cleandata['MO']['BorS'] == -1)
+sell_no_walk = (cleandata['MO']['APPS'] == cleandata['MO']['BBP'] ) & (cleandata['MO']['BorS'] == 1)
+total_walk = buy_no_walk | sell_no_walk # | is OR operator
+
+print(f" Percentage of orders that did not walk the book for INTC on April 1 2024 is {(total_walk.sum()/(len(cleandata['MO'])) * 100):.2f} % ")
 ######### 
 
-#Maybe add some histograms or other plots for exploratory data analysis
+
+#unknown 2 could very well be what side of the book the order appears on, but prove that below
+#unknown 1 could be if its a visible order, proof that using 84 which is in raw and not in clean
+
+
+print(cleandata["Event"][cleandata["Event"]["Type"] == 70])
+#How many shares are sitting ahead of this in the queue
+#Queue_pos
+
+
+
+
+
+#try to recreate the graph at 11 am
+plots(cleandata, 39600000)
+
+#BASpread seems to not be relevant for INTC since the spread is pretty much one cent for the whole day
+plot_feature(regressormatrix, "TotalVolImbalance")
+
+plot_corr_map(regressormatrix)
+
 
 #Starting logistic regression
 
 #Must train model on filtered Data, but can search for its outcome on full data in terms of time, i.e an order might still get filled or not after 3:30 PM
 #Filtered is already above, here below is not constrained on time but still constrained on not including 88 and 84
+
+file_path2 = '../STOCKS/INTC_NASDAQ/INTC_20140424_NASDAQ.mat'
+file_path_MO2 = '../STOCKS/INTC_NASDAQ/Market Order/INTC_20140424.mat'
+rawdata2 = import_data(file_path2, file_path_MO2)
+cleandata2 = clean_data(rawdata2)
+regressormatrix2 = data_regressors(rawdata2, cleandata2)
+
 
 #Below prints how many 1s and 0s we had
 print("\n Total Fills vs Cancels")
@@ -194,52 +232,72 @@ print(regressormatrix["Fill_NoFill"].value_counts())
 #Below prints how many counts of Types we had
 print(cleandata["Event"]["Type"].value_counts())
 
-# print(Clean_Regression_Data.head())
+print(regressormatrix.head())
 
-# #Logistic regression using statsmodels lib
+#Logistic regression using statsmodels lib
+lr_model = LogisticRegression()
+scalar = StandardScaler()
 
-# y = Clean_Regression_Data["Fill_NoFill"]
+y_train = regressormatrix["Fill_NoFill"]
+features = ['QImbalance', 'AbsQImbalance', 'TotalVolImbalance', 'Weighted Vol Imbalance', 
+            'Midprice', 'Microprice'] 
+X_train = regressormatrix[features]
 
-# #Need to add a mandatory col of 1s for intercept
+X_train_standardised = scalar.fit_transform(X_train) #Here we fit and transform
+#Fit Scikit logistic regrssion
 
-# Clean_Regression_Data = sm.add_constant(Clean_Regression_Data)
+lr_model.fit(X_train_standardised, y_train)
 
-# X = Clean_Regression_Data.drop(columns = ["TOD", "Fill_NoFill"])
+y_true = regressormatrix2["Fill_NoFill"]
 
-# #Standardize, dont need to standardize the first col since its all 1s
+X_test = regressormatrix2[features]
 
-# X.iloc[:, 1:] = (X.iloc[:, 1:] - X.iloc[:, 1:].mean())/X.iloc[:, 1:].std() 
+X_test_scaled = scalar.transform(X_test) #Here we transform and not fit anymore i.e we use same scale as above so comparisons are valid
 
-
-# logit_mod = sm.Logit(y, X).fit()
-
-# print(logit_mod.summary())
-
-# #Do some prediction using scikit learn
-# #Baseline fill percentage which i defined as the number of ones divided by number of ones and zeros in fill_map, which guarantees uniqueness by the fact i used .last in code before it
-# print(f"Baseline Fill percentage is {fill_map.mean()*100} %")
-
-# #Fill prediction from logit model
-
-# #predict prob of a fill for each row entry in X
-
-# # predict_fill_val = logit_mod.predict(X)
-
-# # #When does the logit model classify fill or no
-# # trading_treshold = 0.08
-# # # i.e it works like this
-# # # testdf = pd.DataFrame([True, False])
-# # # print(testdf)
-# # # print(testdf.astype(int))
-# # predictions_binary = (predict_fill_val > trading_treshold).astype(int)
-
-# # conf_matrix = confusion_matrix(y, predictions_binary)
-# # print(conf_matrix)
-
-# # print(classification_report(y, predictions_binary))
+#Do some prediction using scikit learn
+y_pred = lr_model.predict(X_test_scaled)
+y_pred_prob = lr_model.predict_proba(X_test_scaled)[: , 1] # We are now only looking at the fill probabilities
 
 
-# # precisions, recalls, thresholds = precision_recall_curve(y, predict_fill_val)
+
+
+
+
+
+
+
+
+model_coef_df = pd.DataFrame(
+    
+    {
+     "Feature": features,
+     "Coefficient (Log Odds)" : lr_model.coef_[0],  # We only predict binary classification so our model only has one row so access that with [0]
+     "Odds Ratio": np.exp(lr_model.coef_[0])
+     }
+    
+    )
+print(model_coef_df.sort_values(by = "Odds Ratio", key=abs, ascending=False))
+
+
+#Baseline fill percentage which i defined as the number of ones divided by number of ones and zeros in fill_map, which guarantees uniqueness by the fact i used .last in code before it
+print(f"Baseline Fill percentage is {regressormatrix2['Fill_NoFill'].mean()*100:.2f} %")
+
+
+
+print("Engine metrics")
+
+brierscore = brier_score_loss(y_true, y_pred_prob)
+print(f'Brier score is {brierscore:.3f}')
+
+logloss = log_loss(y_true, y_pred_prob)
+print(f'Logloss score is {logloss:.3f}')
+
+aucscore = roc_auc_score(y_true, y_pred_prob)
+print(f'AUC score is {aucscore:.3f}')
+
+
+#######Work on LOB chronological order reconstruction 
+    
 
 
 
